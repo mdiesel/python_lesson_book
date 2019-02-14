7d4a4ced36959576627b5ca69544001f518cfee3
mdiesel <mdiesel@yandex.ru>
2019-02-14 18:30:55 +0300

    Control_programm

--- python_book.py
+++ python_book.py
@@ -228,8 +228,29 @@ D: 224-239
 
 """
 
+from ipaddress import IPv4Address
 
+ipv4_addr = False
 
+while not ipv4_addr:
+    address = input("Введите IP адрес в десятично-точечном формате: ")
+    try:
+        ipv4_addr = IPv4Address(address)
+    except ValueError:
+        print('Incorrect IPv4 address.')
+
+first = int(str(ipv4_addr).split('.')[0])
+
+if ipv4_addr.is_multicast:
+    print('multicast')
+elif str(ipv4_addr) == '255.255.255.255':
+    print('local broadcast')
+elif str(ipv4_addr) == '0.0.0.0':
+    print('unassigned')
+elif first >= 0 and first < 224:
+    print('unicast')
+else:
+    print('unused')
 
 
 
